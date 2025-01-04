623a7cd9224653211d2d358973fc8f70f27ff8db
Daniel Bengl <daniel.bengl@renuo.ch>
2025-01-04 22:10:08 +0100

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{fs::File, io::stdout, iter::once};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "./test-files/cat.bmp";
+    let bytes = std::fs::read(file_path).unwrap();
+
+    print_buffer(bytes);
+}
+
+fn print_buffer(bytes: Vec<u8>) {
+    let string_bytes: Vec<String> = bytes.into_iter().map(|byte| to_binary(byte)).collect();
+    let output_string = string_bytes.join(" ");
+    print!("{}", output_string)
+}
+
+fn to_binary(decimal: u8) -> String {
+    let mut decimal = decimal;
+    let mut digits = [0; 8];
+    let mut index = 0;
+
+    while decimal > 0 {
+        digits[index] = decimal % 2;
+        digits[2] = 4;
+        decimal /= 2;
+
+        index += 1;
+    }
+
+    let mut string = String::new();
+    digits.into_iter().for_each(|v| string += &v.to_string());
+
+    string
 }
